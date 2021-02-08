92d0b2110524e1f5c3004c266b2a49cc8b0427f1
Andy Brace <git-hub@openbook.uk.com>
2021-02-08 17:23:12 +0000

    updating install (led + cron)
    turn off led & correctly set cron

--- install.sh
+++ install.sh
@@ -8,13 +8,14 @@ curl https://get.pimoroni.com/inky | bash
 
 ## Fetch the display repo code
 git clone https://github.com/openbook/shouldi-eink-display.git
-cd shouldi-eink-display
+cd /home/pi/shouldi-eink-display
 
 ### pi specific pandas
 pip3 install pytz
 # Add cron schedule
-line="*/30 * * * * $(pwd)/display.py"
-echo $line
+sudo touch /var/spool/cron/pi
+sudo /usr/bin/crontab /var/spool/cron/pi
+line="5,35 * * * * cd $(pwd) && python3 display.py"
 (crontab -u pi -l; echo "$line" ) | crontab -u pi -
 
 # Save a tiny bit more power by turning off the LED
